59fe0318d70439092f3ed4147160a83991dc86c8
Jacob Walton <waltojac@eos11.cis.gvsu.edu>
2018-02-12 15:26:56 -0500

    Layout written.

--- project2/program2/program.c
+++ project2/program2/program.c
@@ -8,7 +8,45 @@
 #include <string.h>
 
 int main(int argc, char * argv[]){
+	
+	//accept 2 file names
+	
 
+
+	//spawn Searchers
+	
+
+	//loop 
+	while(1){
+		
+		//take in search string
+		
+
+
+		//send string to Searchers via Pipe
+		
+
+		//report statistics
+		
+
+		//conduct another search?
+	
+		
+	}
+
+	//graceful shutdown
 
 }
 
+
+void* Searcher(void* args){
+	
+	//open file
+	
+
+	//read + search file
+	
+
+	//send to Master via pipe
+
+}
