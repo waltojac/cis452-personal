b0b77394838fd971db8d4c796ee8a24107bdc5fc
Jacob Walton <waltojac@eos11.cis.gvsu.edu>
2018-02-13 14:39:16 -0500

    More work.

--- project2/program2/program.c
+++ project2/program2/program.c
@@ -9,41 +9,35 @@
 
 //function declarations
 void grabNames();
+void Searcher(int i);
+void spawnChildren();
+
 
 //variables
 char first[50];
 char second[50];
-
+int numFiles = 0;
 char ** fileNames;
-
+int child_pid[10];
 
 int main(int argc, char * argv[]){
 
 	//create fileNames array
 	fileNames = (char**)malloc(sizeof(char*)*500);
 	
-	
-
-	int i = 0;
 	//allocate memory
+	int i = 0;
 	for (i = 0; i < 11; i++){
 		fileNames[i] = (char*)malloc(sizeof(char*));
 	}
 
 	//get the filenames
 	grabNames();
+	printf("%s, %s\n", fileNames[0], fileNames[1]);
 
-	int child1_pid = fork();
-      if(child1_pid < 0) {
-           perror("An error occurred while forking");
-           exit(1);
-       } else if(child1_pid == 0) {    //Child Process
-	     
-
-		 } else {    // parent
-
-
-		 }
+	//spawn Searchers
+	printf("Spawning searchers...\n");
+	spawnChildren();
 
 	//loop 
 	while(1){
@@ -68,7 +62,6 @@ int main(int argc, char * argv[]){
 }
 
 void grabNames(){
-	int numFiles = 0;
 
 	//accept number of files
 	printf("Please enter the number of files to be searched: \n ");
@@ -80,17 +73,27 @@ void grabNames(){
 		printf("Please enter filename %d: \n ", i);
 		fflush(stdin);
 		scanf("%s", fileNames[i]);
-
 	}
+}
 
-	//spawn Searchers
-	printf("Spawning searchers...\n");
-
+void spawnChildren(){
+	int i;
+	for(i = 0; i < numFiles; i++){
+		child_pid[i] = fork();
+      	if(child_pid[i] < 0) {
+       	    perror("An error occurred while forking");
+        	exit(1);
+       	} else if(child_pid[i] == 0) {    //Child Process
+	     	Searcher(i);
+			break;
+		 }
+	}
+	
 }
 
 
-void* Searcher(void* args){
-	
+void Searcher(int i){
+	printf("Child %d searching %s...\n", i, fileNames[i]);
 	//open file
 	
 
@@ -98,7 +101,9 @@ void* Searcher(void* args){
 	
 
 	//send to Master via pipe
-	return args;
+	
+
+	exit(0);
 }
 
 
