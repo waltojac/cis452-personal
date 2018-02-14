9815bb3b5af7cf36b030d78d9d916b366f0356ba
Jacob Walton <waltojac@mail.gvsu.edu>
2018-02-13 21:01:39 -0500

    Ported to CLion project.

---
+++ project2/program2/cmake-build-debug/CMakeFiles/program2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/jacobwalton/Documents/Senior/cis452/cis452Personal/project2/program2/program.c" "/Users/jacobwalton/Documents/Senior/cis452/cis452Personal/project2/program2/cmake-build-debug/CMakeFiles/program2.dir/program.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project2/program2/cmake-build-debug/CMakeFiles/program2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program2.dir/program.c.o"
+  "program2.pdb"
+  "program2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
